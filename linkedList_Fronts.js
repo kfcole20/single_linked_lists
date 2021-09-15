36ce2303933d16e4ce65264668b9bda60196a380
Kevin Cole <kfcole20@gmail.com>
2021-09-15 11:25:23 -0500

    Created addFront, removeFront, and view front methods

--- linkedList_Fronts.js
+++ linkedList_Fronts.js
@@ -1,4 +1,4 @@
-class Node{
+class SLLNode{
     constructor(value){
         this.value=value;
         this.next=null;
@@ -6,13 +6,13 @@ class Node{
 
 }
 
-class SSL{
+class SLL {
     constructor(){
         this.head=null;
     }
     addFront(value){
         //creating the new node
-        newNode= new Node(value)
+        var newNode= new SLLNode(value)
         //creating pointer to current head
         newNode.next= this.head
         //reassigned this newNode as new head
@@ -20,7 +20,38 @@ class SSL{
         return this;
     }
 
+    removeFront()
+    {
+        if( this.head==null){
+            return null;
+        }
+        var currentNode= this.head
+        this.head=currentNode.next
+        return this
+    }
+    
+    front(){
+        if(this.head==null)
+        {
+            return null;
+        }
+        var front = this.head
+        return front.value
+    }
 
-
-
+    
 }
+
+var myList= new SLL()
+var testList= new SLL()
+// console.log(myList)
+myList.addFront(1)
+myList.addFront(2)
+myList.addFront(3)
+myList.addFront(4)
+
+// console.log(myList)
+// myList.removeFront()
+// // console.log(testList.removeFront())
+// console.log(myList)
+console.log(myList.front())
